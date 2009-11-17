06e32997e2fed6a0c1338842e9d344459fdb51d7
suryagullapalli <suryagullapalli@a1ae422f-012c-4595-b219-9b566b4fa53f>
2009-11-17 12:55:40 +0000

    Using shared_ptr of signals in fusion container
    git-svn-id: https://svindia.svn.sourceforge.net/svnroot/svindia@7 a1ae422f-012c-4595-b219-9b566b4fa53f

--- Fusion/vector.cxx
+++ Fusion/vector.cxx
@@ -2,13 +2,18 @@
 #include <string>
 using namespace std ;
 
-#include <boost/signal.hpp>
+#include <boost/signals2.hpp>
 #include <boost/typeof/typeof.hpp>
 
 #include <boost/fusion/container.hpp>
 #include <boost/fusion/sequence/intrinsic.hpp>
 #include <boost/fusion/functional/invocation/invoke.hpp>
 #include <boost/fusion/functional.hpp>
+#include <boost/fusion/include/for_each.hpp>
+#include <boost/utility/value_init.hpp>
+
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
 
 using boost::shared_ptr ;
 using namespace boost ;
@@ -17,16 +22,27 @@ using namespace boost ;
 #define SIGNAL_ARGS       boost::fusion::make_vector
 #define SignalArgsVec     boost::fusion::vector <BOOST_PP_ENUM_PARAMS(MAX_SIGNAL_PARAMS, T)>
 
+#define BOOST_SIGNAL(...) boost::shared_ptr< boost::signals2::signal < __VA_ARGS__ > >
+
 class MySignals
 {
 	private:
 		typedef boost::fusion::vector <
-				boost::signal <void (const int&, const string&)>
-			,	boost::signal <void ()>
-			,	boost::signal <void (const string&)>
-			> SignalsVec ;
+            BOOST_SIGNAL(void(const int&, const string&))
+            , BOOST_SIGNAL(void())
+            , BOOST_SIGNAL(void(const string&))
+            > SignalsVec ;
 
 		SignalsVec m_signals ;
+
+        struct initialize
+        {
+            template <typename T>
+                void operator() (T& p_item) const
+                {
+                }
+
+        } ;
 
 	public:
 		static MySignals* instance (void) 
@@ -49,7 +65,7 @@ class MySignals
 	template <int id, BOOST_PP_ENUM_PARAMS(MAX_SIGNAL_PARAMS, typename T)>        
 		void emitSignal (const SignalArgsVec& p_vector)                           
 			{                                                                     
-				fusion::fused <fusion::result_of::at_c<SignalsVec, id>::type>     
+				fusion::fused <typename fusion::result_of::at_c<SignalsVec, id>::type>     
 				functionObj (signal <id>()) ;                                     
 				functionObj (p_vector) ;                                          
 			}                                                                     
@@ -62,7 +78,12 @@ class MySignals
 
 	private:
 		static MySignals* m_instance ;
-		MySignals () { }
+		MySignals () 
+        { 
+            //fusion::at_c<VoidSignal>(m_signals) = new signal<void()>() ;
+            fusion::at_c<IntStringSignal>(m_signals) = new signal<void(const int&, const string&)>() ;
+            //fusion::at_c<StringSignal>(m_signals) = new signal<void(const string&)>() ;
+        }
 } ;
 
 MySignals* MySignals :: m_instance = (MySignals *)0 ;
@@ -79,7 +100,16 @@ void print_data (const int& p_int, const string& p_string)
 	return ;
 }
 
+void print_void (void)
+{
+    cout << "Void Slot" << endl ;
+    return ;
+}
+
 int main (void)
 {
 	MySignals* sig = MySignals::instance() ;
+#if 0
+    sig->signal<MySignals::VoidSignal>()->connect(boost::lambda::bind(&print_void)) ;
+#endif
 }
