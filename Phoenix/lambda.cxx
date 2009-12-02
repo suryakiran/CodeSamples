dfc05aae0824c9f8b06455197168ef625e9e9a13
suryagullapalli <suryagullapalli@a1ae422f-012c-4595-b219-9b566b4fa53f>
2009-12-02 18:08:14 +0000

    Fix Compilation errors with gcc compiler
    git-svn-id: https://svindia.svn.sourceforge.net/svnroot/svindia@36 a1ae422f-012c-4595-b219-9b566b4fa53f

--- Phoenix/lambda.cxx
+++ Phoenix/lambda.cxx
@@ -1,26 +1,44 @@
-#include "std.hxx"
-#include "phoenix.hxx"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-void trim(string& s)
+using std::cout ;
+using std::endl ;
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace phx = boost::phoenix ;
+namespace pha = phx::arg_names ;
+namespace str_algo = boost::algorithm ;
+
+void trim(std::string& s)
 {
-	boost::algorithm::trim(s) ;
+	str_algo::trim(s) ;
 }
 
 int main (void)
 {
-	vector<string> vs ;
+	std::vector<std::string> vs ;
 
-	vs.push_back (" Surya ") ;
-	vs.push_back (" Kiran ") ;
-	vs.push_back (" Gullapalli ") ;
+	vs.push_back (" String1 ") ;
+	vs.push_back (" String2 ") ;
+	vs.push_back (" String3 ") ;
 
 	phx::for_each (pha::arg1, phx::lambda[ cout << pha::arg1 << '\n' ])(vs) ;
 	cout << "===================================" << endl ;
 
-	//std::for_each (vs.begin(), vs.end(), phx::bind(&trim, pha::arg1));
-	std::for_each (vs.begin(), vs.end(), phx::bind(&str::template trim, pha::_1));
-	//std::for_each (vs.begin(), vs.end(), phx::reverse(pha::arg1));
+	// What I'm trying to do is call boost::algorithm::trim on all elements of vector.
+	// the below given statement works without any problem.
 
+	void (*pf) (std::string&) = &str_algo::trim<std::string> ;
+	std::for_each (vs.begin(), vs.end(), phx::bind(pf, pha::arg1));
+	
+	//How do I call boost::algorithm::trim directly using Boost.Phoenix or Boost.Lambda. Below
+	//I'm trying to do it using phoenix.
+	//std::for_each (vs.begin(), vs.end(), phx::bind(&str_algo::template trim, pha::_1));
+	
 	phx::for_each (pha::arg1, phx::lambda[ cout << pha::arg1 << '\n' ])(vs) ;
 	cout << "===================================" << endl ;
 }
