e89e0eef74acc96c2d69090282c2ff37f310005e
suryagullapalli <suryagullapalli@a1ae422f-012c-4595-b219-9b566b4fa53f>
2010-02-23 07:15:15 +0000

    Modified Build System
    git-svn-id: https://svindia.svn.sourceforge.net/svnroot/svindia@85 a1ae422f-012c-4595-b219-9b566b4fa53f

--- CPP/iterator.cxx
+++ CPP/iterator.cxx
@@ -3,13 +3,17 @@
 namespace {
 	void printVector()
 	{
-		typedef Triplet<int> IntTriplet ;
+		typedef skg::Triplet<int> IntTriplet ;
 
 		vector< IntTriplet > vti ;
 		vti.push_back (IntTriplet (1, 2, 3)) ;
 		vti.push_back (IntTriplet (5, 5, 66)) ;
 
+		const IntTriplet& it = vti[0] ;
+
 		copy (vti.begin(), vti.end(), ostream_iterator<IntTriplet> (cout, "\n")) ;
+
+		cout << it << endl ;
 	}
 }
 int main (void)
--- CPP/triplet.hxx
+++ CPP/triplet.hxx
@@ -1,20 +1,31 @@
 #ifndef TRIPLET_HXX
-#define TRIPLET_HXX
+#define TRIPLET_HXX 1
 
 #include "std.hxx"
 
-template <class T>
-struct Triplet
+namespace skg 
 {
-	T x, y, z ;
-	Triplet (const T& p_x, const T& p_y, const T& p_z)
-		: x(p_x), y(p_y), z(p_z) { }
-} ;
+	template <class T>
+		struct Triplet
+		{
+			private:
+			T x, y, z ;
+
+			public:
+			Triplet (const T& p_x, const T& p_y, const T& p_z)
+				: x(p_x), y(p_y), z(p_z) { }
+
+			void get (T& p_x, T& p_y, T& p_z) const
+			{ p_x = x ; p_y = y; p_z = z ; }
+		} ;
+}
 
 template <class T>
-ostream& operator<< (ostream& os, const Triplet<T>& p_t)
+ostream& operator<< (ostream& os, const skg::Triplet<T>& p_t)
 {
-	os << '(' << p_t.x << ',' << p_t.y << ',' << p_t.z << ')' ;
+	T x, y, z ;
+	p_t.get(x, y, z) ;
+	os << '(' << x << ',' << y << ',' << z << ')' ;
 	return os ;
 }
 #endif
