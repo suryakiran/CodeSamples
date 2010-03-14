fd41a8f059a33a665db7dd13b73033a3bb1937e4
suryagullapalli <suryagullapalli@a1ae422f-012c-4595-b219-9b566b4fa53f>
2010-03-14 19:14:12 +0000

    This test now stores info in pt::ptree.
    git-svn-id: https://svindia.svn.sourceforge.net/svnroot/svindia@100 a1ae422f-012c-4595-b219-9b566b4fa53f

--- FileReading/MoneyControl.cxx
+++ FileReading/MoneyControl.cxx
@@ -12,8 +12,18 @@ int main (int argc, char** argv)
 	fs::fstream fin ;
 	fin.open (inputFile, ios_base::in | ios_base::binary) ;
 
-	set<string> ignoreFields ;
-	insert (ignoreFields) ("Vol") ;
+	set<string> validFields ;
+	insert (validFields) 
+		("Open") ("High") ("Low") ("52 Wk") ("Last Traded Price") 
+		("Prev Close") ("Year High") ("Year Low") ;
+
+	typedef pair<bool, bool> BoolPair ;
+	map<string, BoolPair> fieldCaptured ;
+
+	BOOST_FOREACH (const string& s, validFields)
+		fieldCaptured[s] = make_pair(false, false) ; //First bse, second nse ;
+
+	fieldCaptured.erase("52 Wk") ;
 
 	xpr::smatch results ;
 	string l ;
@@ -23,22 +33,37 @@ int main (int argc, char** argv)
 	pt::ptree& nsePrices = tree.put_child ("Scrip.PriceDetails.Nse", pt::ptree()) ;
 	pt::ptree& bsePrices = tree.put_child ("Scrip.PriceDetails.Bse", pt::ptree()) ;
 
+	nsePrices.put ("Last Traded Price", 0.0) ;
+	bsePrices.put ("Last Traded Price", 0.0) ;
+
 	xpr::sregex overview =
 		as_xpr("<b>") >> *_s >> "Overview" >> *_s >> "</b>" ;
 
 	xpr::sregex validText =
 		as_xpr(*(~(xset= '<','&',';'))) ;
 
+	xpr::sregex spanReg = 
+		as_xpr('<') >> *_s >> "span" >> *_s >> "class" >> *_s >> '=' >> *_s
+		>> quotedWord >> *_s >> '>' >> (priceTag= textBeforeAnchor) >> "</span>" ;
+
+	xpr::sregex strongDetails =
+		as_xpr("\"det\"") >> *_s >> '>' >> *_s >> '<' >> *_s >> "strong" >> *_s ;
+
+	xpr::sregex fieldTag =
+		as_xpr("\"hed\"") | strongDetails ;
+
 	xpr::sregex td =
-		as_xpr("<td class") >> *_s >> "=" >> *_s >> "\"hed\"" >> '>'
-		>> (textTag= validText) >> "</td>" ;
+		as_xpr("<td class") >> *_s >> "=" >> *_s >> fieldTag >> '>'
+		>> *_s >> (textTag= validText) ;
 
 	xpr::sregex price =
 		as_xpr("<td") >> *(~(xset= '>')) >> '>' >> *_s
-		>> (priceTag = +(_d|numberDelims)) ;
+		>> (priceTag = *(_d|numberDelims)) >> *_s >> '<' ;
 
 	bool priceDetailsBegin (false) ;
 	bool bseDetails (true) ;
+	string prevField ("") ;
+
 	while (getline (fin, l, '\n'))
 	{
 		IGNORE_BLANK_LINE(l) ;
@@ -49,11 +74,41 @@ int main (int argc, char** argv)
 		if (!priceDetailsBegin)
 			continue ;
 
-		if (xpr::regex_match (l, results, td))
+		if (xpr::regex_search (l, results, spanReg))
+		{
+			string field ("Last Traded Price") ;
+			string s (results[priceTag]) ;
+			str::trim(s) ;
+			str::erase_all (s, ",") ;
+			double price (0.0) ;
+			try { price = lexical_cast<double>(s) ; }
+			catch (...) { }
+			if (!fieldCaptured[field].first)
+			{
+				bsePrices.put(field, price) ;
+				fieldCaptured[field].first = true ;
+			}
+			else if (!fieldCaptured[field].second)
+			{
+				nsePrices.put(field, price) ;
+				fieldCaptured[field].second = true ;
+			}
+		}
+
+		if (xpr::regex_search (l, results, td))
 		{
 			string field (results[textTag]) ;
-			if (!ignoreFields.count(field))
+			str::trim(field) ;
+			str::erase_all(field, ".") ;
+			if (validFields.count(field))
 			{
+				string currentField (field) ;
+				if (prevField == "High")
+					currentField = "Year High" ;
+				if (prevField == "Low")
+					currentField = "Year Low" ;
+
+				prevField = field ;
 				while (getline (fin, l, '\n'))
 				{
 					IGNORE_BLANK_LINE(l) ;
@@ -61,15 +116,20 @@ int main (int argc, char** argv)
 					{
 						string s (results[priceTag]) ;
 						str::erase_all (s, ",") ;
-						if (bseDetails)
+
+						double price (0.0) ;
+						try { price = lexical_cast<double>(s); }
+						catch (...) { }
+
+						if (!fieldCaptured[currentField].first)
 						{
-							bsePrices.put(field, lexical_cast<double>(s)) ;
-							bseDetails = false ;
+							bsePrices.put(currentField, price) ;
+							fieldCaptured[currentField].first = true ;
 						}
-						else
+						else if (!fieldCaptured[currentField].second)
 						{
-							nsePrices.put(field, lexical_cast<double>(s)) ;
-							bseDetails = true ;
+							nsePrices.put(currentField, price) ;
+							fieldCaptured[currentField].second = true ;
 						}
 
 						break ;
