2c11bfd15026b8940037e77ebd73e32e9e55746a
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-10 09:27:31 +0530

    Initial Revision. Skeleton files and setup.

---
+++ Python/CallPython.cxx
@@ -0,0 +1,7 @@
+#include <Python.h>
+#include <std.h>
+
+int main (int argc, char** argv)
+{
+  cout << "Call Python" << endl;
+}
---
+++ std.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <deque>
+#include <sstream>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
