49b022edd34534459cf0220287e153365892104f
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-30 14:32:01 +0530

    Changes to take care of pointer also during sv_object_cast

--- PerlCpp/SV2Obj.hxx
+++ PerlCpp/SV2Obj.hxx
@@ -1,19 +1,25 @@
 #ifndef SV2Obj_hxx_INCLUDED
 #define SV2Obj_hxx_INCLUDED
 
+#include <boost/type_traits/remove_pointer.hpp>
+#include <boost/type_traits/add_pointer.hpp>
+
 template <class TargetType>
 class sv_object_cast
 {
+  typedef typename boost::remove_pointer<TargetType>::type target_type;
+  typedef typename boost::add_pointer<target_type>::type target_pointer;
+
   public:
     sv_object_cast (SV* p_perlSv)
       : m_sv(p_perlSv), m_target(0)
     {
     }
 
-    TargetType* operator() ()
+    target_pointer operator() ()
     {
       if (sv_isobject(m_sv) && (SvTYPE(SvRV(m_sv)) == SVt_PVMG)) {
-        m_target = (TargetType *)SvIV((SV*)SvRV(m_sv));
+        m_target = (target_pointer)SvIV((SV*)SvRV(m_sv));
       }
 
       return m_target;
@@ -21,7 +27,7 @@ class sv_object_cast
 
   private:
     SV* m_sv;
-    TargetType* m_target;
+    target_pointer m_target;
 };
 
 #endif
