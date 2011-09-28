b4ffe6a9edc5f42c50de35acc384ceb0390f9e11
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-09-28 17:52:31 +0530

    Working version of Nary example along with startup script

--- Signals/Nary.cxx
+++ Signals/Nary.cxx
@@ -2,7 +2,14 @@
 #include <algorithm>
 #include <Signals/Signals.hxx>
 #include <Signals/BoostSlots.hxx>
+#include <boost/typeof/typeof.hpp>
+#include <boost/function_types/function_arity.hpp>
+#include <boost/function_types/function_type.hpp>
+#include <boost/function_types/parameter_types.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/add_pointer.hpp>
 using namespace std;
+namespace bfun = boost::function_types;
 
 class Signal : public SignalBase
 {
@@ -25,11 +32,14 @@ struct Slot
   Slot() { }
 
   virtual void voidSlot () { cout << "Slot::voidSlot" << endl; }
+  virtual void argSlot(int) { }
 };
 
 struct SlotDerived : public Slot
 {
-  SlotDerived() { }
+  SlotDerived() { 
+    cout << typeid (BOOST_TYPEOF(this)).name() << endl;
+  }
 
   virtual void voidSlot () { cout << "SlotDerived::voidSlot" << endl; }
 };
@@ -43,6 +53,13 @@ int main (void)
   sig.signal<Signal::VoidSignal>().connect(MEM_FUN_OBJ_PTR (Slot,voidSlot,slot,0));
 
   sig.emitSignal<Signal::VoidSignal>();
+  typedef BOOST_TYPEOF (slot) slotType;
+  cout << typeid(slotType).name() << endl;
+  typedef BOOST_TYPEOF (&Slot::argSlot) funcType;
+  cout << bfun::function_arity<funcType>::value << endl;
+  typedef boost::mpl::at_c<bfun::parameter_types<funcType>, 0>::type arg0Type;
+  typedef boost::add_pointer<arg0Type>::type arg0pType;
+  cout << boost::is_same<arg0pType, slotType>::value << endl;
 
   cout << "--------------------------------" << endl;
 }
