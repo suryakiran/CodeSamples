b3bb9608492623001a248f11e052962de0e3a200
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-11-23 09:24:57 +0530

    Algorithms. Initial version

---
+++ Algorithms/swap-array.cxx
@@ -0,0 +1,20 @@
+#include <std.hxx>
+
+int main (void)
+{
+  int a[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+  int N = sizeof(a)/sizeof(int);
+  int n = N/2;
+
+  printContainer ("Before Swap:", a);
+
+  for (int i = 1; i <= n; ++i)
+  {
+    for (int j = i; j <= n; ++j)
+    {
+      swap (a[j], a[n+j]);
+    }
+  }
+
+  printContainer ("After Swap:", a);
+}
