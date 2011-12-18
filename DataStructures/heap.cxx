d86711b57683bdc951788134475b80403d2e5c5c
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-12-18 22:43:25 +0530

    Heap Sort implemented

--- DataStructures/heap.cxx
+++ DataStructures/heap.cxx
@@ -5,9 +5,11 @@ int main (void)
   vector<int> vi;
   vi += 9, 8, 10, 15, 7, 22, 25, 30 ;
   heap<int> h (vi);
-  h.print();
+  h.print("Initial Heap");
   h.add (6);
-  h.print();
+  h.print("Added 6");
   h.add (1);
-  h.print();
+  h.print("Added 1");
+  h.sort();
+  h.print("Sorted Heap");
 }
