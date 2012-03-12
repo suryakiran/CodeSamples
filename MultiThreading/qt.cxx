bf85a7f6565381e23cbd39235ff3b8b338c7f04f
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2012-03-12 11:28:26 +0530

    Use Vcxproj.user file to run the built executables on windows

--- MultiThreading/qt.cxx
+++ MultiThreading/qt.cxx
@@ -3,7 +3,6 @@
 
 int main (int argc, char** argv)
 {
-  QApplication::setAttribute (Qt::AA_X11InitThreads);
   QApplication app (argc, argv);
   MainWindow *mainw = new MainWindow;
   mainw->show();
