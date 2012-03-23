fca1a147898126a0a9734e3204ca28de0e5efdff
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2012-03-23 09:34:13 +0530

    Lighting effects

--- OpenSceneGraph/sphere.cxx
+++ OpenSceneGraph/sphere.cxx
@@ -2,6 +2,8 @@
 #include <osg/Group>
 #include <osg/Geometry>
 #include <osg/ShapeDrawable>
+#include <osg/StateSet>
+#include <osg/Material>
 #include <osgViewer/Viewer>
 #include <osgGA/TrackballManipulator>
 
@@ -15,6 +17,18 @@ int main ()
   osg::ShapeDrawable* sphereGeom = new osg::ShapeDrawable (shape);
   sphere->addDrawable (sphereGeom);
   root->addChild (sphere);
+
+  osg::StateSet* state = sphere->getOrCreateStateSet();
+  osg::Light* light = new osg::Light (0);
+  light->setDiffuse(osg::Vec4(1.0, 0.8, 0.8, 1.0));
+  light->setSpecular(osg::Vec4(0.0, 0.0, 1.0, 1.0));
+  state->setAttributeAndModes (light);
+
+  osg::Material* mat = new osg::Material();
+  mat->setDiffuse (osg::Material::BACK, osg::Vec4(1.0, 1.0, 0.0, 1.0));
+  mat->setSpecular (osg::Material::FRONT, osg::Vec4(0.0, 0.0, 1.0, 1.0));
+  mat->setShininess (osg::Material::FRONT, 125.0);
+  state->setAttributeAndModes (mat);
 
   viewer.setSceneData(root);
   viewer.run();
