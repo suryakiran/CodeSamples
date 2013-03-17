1768c134b79755596f934926b39ea34c7b095433
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2013-03-17 17:22:18 +0530

    Fusion samples working with Boost-1.53.0

--- Fusion/map.cxx
+++ Fusion/map.cxx
@@ -1,7 +1,7 @@
 #include "std.hxx"
 
 #include <boost/typeof/typeof.hpp>
-#include <boost-spirit.hxx>
+// #include <boost-spirit.hxx>
 
 #include <boost/fusion/container.hpp>
 #include <boost/fusion/sequence/intrinsic.hpp>
@@ -11,20 +11,57 @@
 namespace fusion = boost::fusion;
 
 #include <boost/shared_ptr.hpp>
-using boost::shared_ptr ;
 
 #include <boost/type_traits/is_same.hpp>
 #include <boost/type_traits/remove_reference.hpp>
+#include <boost/type_traits/add_reference.hpp>
+
+#include <boost/preprocessor/repetition/enum_shifted_binary_params.hpp>
 
 #include <boost/signals2.hpp>
 namespace signals = boost::signals2 ;
-#define BOOST_SIGNAL(...) shared_ptr< signals::signal < __VA_ARGS__ > >
+#define BOOST_SIGNAL(...) boost::shared_ptr< signals::signal < __VA_ARGS__ > >
 
 #define DEF_STRUCT(name) struct name { }
 
 #ifndef MAX_SIGNAL_PARAMS
 #define MAX_SIGNAL_PARAMS 10
-#endif 
+#endif
+
+namespace detail {
+  template <class Class>
+  struct has_typedef
+  {
+    typedef char yes[1];
+    typedef char no[2];
+    
+    template <class T>
+    static yes& test(typename T::element_type*);
+    
+    template <class T>
+    static no& test(...);
+    static const bool value = sizeof(test<Class>(0)) == sizeof(yes);
+  };
+
+  template <class SmartPtrType, bool b>
+  struct element_type_p {
+    typedef typename SmartPtrType::element_type value_type;
+    typedef typename boost::add_reference<value_type>::type reference_type;
+  };
+
+  template <class SmartPtrType>
+  struct element_type_p<SmartPtrType, false> {
+    typedef typename SmartPtrType::value_type value_type;
+    typedef typename SmartPtrType::reference_type reference_type;
+  };
+  
+  template <class SmartPtrType>
+  struct element_type {
+    static const bool value = has_typedef<SmartPtrType>::value;
+    typedef typename element_type_p<SmartPtrType, value>::value_type value_type;
+    typedef typename element_type_p<SmartPtrType, value>::reference_type reference_type;
+  };
+}
 
 class SigClass
 {
@@ -49,8 +86,8 @@ class SigClass
 			struct get_type
 			{
 				typedef typename boost::remove_reference <
-					typename fusion::result_of::at_key<FusionMap, T>::type>::type
-					::reference value_type;
+					typename fusion::result_of::at_key<FusionMap, T>::type>::type smart_ptr_type;
+        typedef typename detail::element_type<smart_ptr_type>::reference_type reference_type;
 			} ;
 
 		struct initialize 
@@ -59,7 +96,7 @@ class SigClass
 				void operator() (FusionMapItem& p_item) const
 				{
 					typedef typename FusionMapItem::second_type SmartPtr;
-					typedef typename SmartPtr::value_type ItemType ;
+          typedef typename detail::element_type<SmartPtr>::value_type ItemType;
 					p_item.second = SmartPtr(new ItemType) ;
 				}
 		} ;
@@ -73,7 +110,7 @@ class SigClass
 		bool blockAllSignals (void) const { return m_blockAllSignals ; }
 
 		template <class T> 
-			typename get_type<T>::value_type
+			typename get_type<T>::reference_type
 				signal (void)
 				{
 					return *(fusion::at_key<T>(fm));
@@ -85,7 +122,7 @@ class SigClass
             BOOST_PP_IF(n, BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS, void BOOST_PP_TUPLE_EAT(3))\
             (BOOST_PP_INC(n), const T, &var))\
 	{\
-		boost::fusion::fused<typename get_type<id>::value_type> functionObj (signal<id>()) ;\
+		boost::fusion::fused<typename get_type<id>::reference_type> functionObj (signal<id>()) ; \
 		if (blockAllSignals()) return ;\
 		functionObj (boost::fusion::make_vector(BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(n), var))) ;\
 	}
@@ -120,7 +157,7 @@ int main (void)
 
 	sc.blockAllSignals(false);
 
-	sc.blockSignal<SigClass::VoidSignal> (true);
+	// sc.blockSignal<SigClass::VoidSignal> (true);
 
 	sc.emitSignal<SigClass::VoidSignal>();
 	sc.emitSignal<SigClass::IntSignal>(5);
