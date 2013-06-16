8f1c288321d5a26e8650062f56df72897e3f6559
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2013-06-16 18:13:46 +0530

    Authenticate user for mongo database

--- MongoDB/first.cxx
+++ MongoDB/first.cxx
@@ -4,16 +4,22 @@
 void run()
 {
   mongo::DBClientConnection c;
-  c.connect("localhost");
+  mongo::HostAndPort host("ds027708.mongolab.com:27708");
+  string error;
+  try {
+    c.connect(host, error);
+    if (c.auth("dummy", "readwriteuser", "readwriteuserabc", error)) {
+      cout << "Authenticated" << endl;
+    } else {
+      cout << "Authentication Failed" << endl;
+    }
+  } catch (const mongo::DBException& e) {
+    cout << "Exception: " << e.what() << endl;
+  }
 }
 
 int main (void)
 {
-  try {
-    run();
-    cout << "Connected Ok" << endl;
-  } catch (const mongo::DBException& e) {
-    cout << "Caught Exception: " << e.what() << endl;
-  }
+  run();
   return 0;
 }
