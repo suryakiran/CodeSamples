8f3ea98607463c9b0a2ed172411a50e946e477aa
Surya Gullapalli <surya.gullapalli@ddu-india.com>
2016-01-27 06:41:54 +0530

    Removed unnecessary build files

--- Json/CMakeFiles/json.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
